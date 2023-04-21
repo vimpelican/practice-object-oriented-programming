19d2ee0bf03df85425491ed7de72ba2915239712
vimpelican <snustcice@g.seoultech.ac.kr>
2023-04-21 21:42:54 +0900

    Fix last char(e) to be splitted

--- lecture_230407/Problem_3.java
+++ lecture_230407/Problem_3.java
@@ -34,24 +34,28 @@ public class Problem_3 {
     */
 
     public static String[] split(String s, String regex) {
-        // 쪼갠 문자열을 저장할 임시 String 배열
+        // 쪼갠 문자열을 저장할 String 배열
         String[] result = new String[s.length()];
+        // substring을 저장할 string
+        StringBuilder temp = new StringBuilder();
+        
         int resultIndex = 0;
-        int lastRegexIndex = -1;
 
         for(int i = 0; i < s.length(); i++) {
+            // regex가 아닌 경우(== 문자인 경우)
+            if(!isRegex(s.charAt(i), regex)) {
+                temp.append(s.charAt(i));
+                result[resultIndex] = temp.toString();
+            }
+
             // regex를 발견한 경우
-            if(isRegex(s.charAt(i), regex)) {
-                // 발견한 regex 전까지의 문자열을 substring으로 전환하여 할당
-                result[resultIndex] = s.substring(lastRegexIndex + 1, i);
-                // 다음 인덱스로 넘어감
+            else {
+                // 문자열 다음 인덱스에 regex를 대입한다
+                result[++resultIndex] = s.charAt(i) + "";
                 resultIndex++;
-                // regex를 저장
-                result[resultIndex] = s.charAt(i) + "";
-                // 다음 인덱스로 넘어감
-                resultIndex++;
-                // 다음 substring을 계산하기 위해 이전에 나왔던 regex의 인덱스 기억
-                lastRegexIndex = i;
+
+                // temp를 초기화한다
+                temp.delete(0, temp.length());
             }
         }
         return result;
